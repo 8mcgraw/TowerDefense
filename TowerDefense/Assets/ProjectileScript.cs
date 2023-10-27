d1d13c5791a17a91f7a03a853205139f136ac525
8mcgraw <123113246+8mcgraw@users.noreply.github.com>
2023-10-27 13:30:06 -0400

    Projectile Progress

--- TowerDefense/Assets/ProjectileScript.cs
+++ TowerDefense/Assets/ProjectileScript.cs
@@ -22,11 +22,12 @@ public class ProjectileScript : MonoBehaviour
         if (activate == true){
             this.gameObject.SetActive(true);
                 //transform.LookAt(newTarget.transform);
-                this.gameObject.transform.localScale = new Vector3(0.6f, 0.5f*Vector3.Distance(this.transform.parent.position, newTarget.transform.position), 0.49f);
+                this.gameObject.transform.localScale = new Vector3(0.25f, 0.25f, 0.3f*Vector3.Distance(this.transform.parent.position, newTarget.transform.position));
                 //this.gameObject.transform.localScale = new Vector3(0.6f, i%10, 0.49f);
                 //Vector3.Lerp(transform.localScale, transform.localScale *10* (Vector3.Distance(transform.position, newTarget.transform.position)), i / 10);
                 //Vector3(0.6, x, 0.49)
-                this.gameObject.transform.position = Vector3.Lerp(transform.parent.position, newTarget.transform.position, 0.5F);
+                this.gameObject.transform.localPosition = new Vector3(0f, 0f, (Vector3.Lerp(transform.parent.position, newTarget.transform.position, 0.5F)).y);
+                //this.gameObject.transform.position = new Vector3(0f, Vector3.Distance(transform.parent.position, newTarget.transform.position), 0f);
                 //Bullet: this.gameObject.transform.position = Vector3.Lerp(this.transform.parent.position, newTarget.transform.position, i % 10);
                 i++;
         } else {
